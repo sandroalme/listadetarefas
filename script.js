d52793011e80998e78d595881c8d9ce897bdf855
sandroalme <111318914+sandroalme@users.noreply.github.com>
2023-06-09 22:57:36 -0300

    começando JavaScript

---
+++ script.js
@@ -0,0 +1,51 @@
+const inputTarefa = document.querySelector('.input_tarefas');
+const btnTarefa = document.querySelector('.btn_add');
+const tarefas = document.querySelector('.tarefas');
+
+function criaDiv() {
+    const div = document.createElement('div');
+    return div;
+}
+
+function criaLi() {
+    const li = document.createElement('li');
+    return li;
+}
+
+inputTarefa.addEventListener('keypress', (e)=> {
+    if (e.keyCode === 13) {
+        if (!inputTarefa.value) return;
+        criaTarefa(inputTarefa.value);
+    }
+});
+
+function limpaInput() {
+    inputTarefa.value = '';
+}
+
+function criaTarefa(textoInput) {
+    const div = criaDiv();
+    div.classList.add('div_li')
+    const li = criaLi();
+    li.innerText = textoInput;
+    const botao = document.createElement("button");
+    botao.innerText = 'apagar';
+    botao.classList.add('btn_remove');
+    tarefas.appendChild(div);
+    div.appendChild(li);
+    div.appendChild(botao);
+    limpaInput();
+}
+
+btnTarefa.addEventListener('click', ()=> {
+    if (!inputTarefa.value) return;
+    criaTarefa(inputTarefa.value);
+});
+
+document.addEventListener('click', (e)=> {
+    const elemento = e.target;
+
+    if (elemento.classList.contains('btn_remove')) {
+        elemento.parentElement.remove();
+    }
+});
